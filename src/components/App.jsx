2318f378ef7dccd3de1238009523b00ff0d2917e
DimaMarkow <dimamarkow@gmail.com>
2023-08-04 13:07:36 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { Profile } from 'components/Profile/Profile';
+// import Statistics from 'components/Statistics/Statistics';
+// import FriendList from 'components/FriendList/FriendList';
+// import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+
+import user from 'data/user.json';
+// import statData from 'data/data.json';
+// import friends from 'data/friends.json';
+// import transactions from 'data/transactions.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile user={user} />
+      {/* <Statistics stats={statData} />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} /> */}
     </div>
   );
 };
